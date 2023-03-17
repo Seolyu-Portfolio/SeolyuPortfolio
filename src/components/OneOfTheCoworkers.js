65aff5fc7308e2933b4e2c89eaa2174bf793c8ef
LeeSeolYu <leeseolyu@gmail.com>
2023-03-17 20:18:08 +0900

    modify coworker page

--- src/components/OneOfTheCoworkers.js
+++ src/components/OneOfTheCoworkers.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import styles from './OneOfTheCoworkers.module.css';
+import React from "react";
+import styles from "./OneOfTheCoworkers.module.css";
 
-function OneOfTheCoworkers({ coworkers }) {
+function OneOfTheCoworkers({ coworkers, onCoworkerClick }) {
+  const handleCoworkerClick = (coworker) => {
+    onCoworkerClick(coworker);
+  };
+
   return (
     <ul className={styles.coworkerList}>
       {coworkers.map((coworker) => (
-        <li key={coworker.id} className={styles.coworker}>
-          <div>
-            <img src={coworker.avatar} alt={coworker.avatar} />
-            <div>{coworker.status}</div>
+        <li
+          key={coworker.id}
+          className={styles.coworker}
+          onClick={() => handleCoworkerClick(coworker)}
+        >
+          <div className={styles.profile}>
+            <img
+              className={styles.avatar}
+              src={coworker.avatar}
+              alt={coworker.avatar}
+            />
+            <div
+              className={`${styles.circle} ${
+                coworker.status === "Online" ? styles.filled : ""
+              }`}
+            ></div>
           </div>
-          <div>{coworker.name}</div>
+          <div className={styles.name}>{coworker.name}</div>
         </li>
       ))}
     </ul>
--- src/pages/Coworker.js
+++ src/pages/Coworker.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import OneOfTheCoworkers from '../components/OneOfTheCoworkers';
-import styles from './Coworker.module.css';
+import React, { useState } from "react";
+import OneOfTheCoworkers from "../components/OneOfTheCoworkers";
+import styles from "./Coworker.module.css";
 
 function Coworker() {
+  const [selectedCoworker, setSelectedCoworker] = useState(null);
+
   const coworkers = [
     {
       id: 1,
-      name: 'Sy',
-      status: 'Online',
-      avatar: '../assets/profile.png',
+      name: "Sy",
+      status: "Online",
+      avatar: "/profile.png",
+      comment: "I would like to recommend my ex-colleague 'Seolyu'",
     },
     {
       id: 2,
-      name: 'Ys',
-      status: 'Offline',
-      avatar: '../assets/profile.png',
+      name: "Ys",
+      status: "Offline",
+      avatar: "/profile.png",
+      comment: "안녕2",
     },
     {
       id: 3,
-      name: 'Le',
-      status: 'Online',
-      aavatar: '../assets/profile.png',
+      name: "Le",
+      status: "Online",
+      avatar: "/profile.png",
+      comment: "She is the best programmer",
     },
   ];
+
+  const handleCoworkerClick = (coworker) => {
+    setSelectedCoworker(coworker);
+  };
 
   return (
     <div className={styles.Coworker}>
@@ -31,9 +40,65 @@ function Coworker() {
         <div className={styles.contents}>
           <div className={styles.sidebar}>
             Colleague Evaluations
-            <OneOfTheCoworkers coworkers={coworkers} />
+            <OneOfTheCoworkers
+              coworkers={coworkers}
+              onCoworkerClick={handleCoworkerClick}
+            />
           </div>
-          <div className={styles.content}>안녕</div>
+          {selectedCoworker ? (
+            <div className={styles.content}>
+              <div className={styles.profileCard}>
+                <div className={styles.profile}>
+                  <img
+                    className={styles.avatar}
+                    src={selectedCoworker.avatar}
+                    alt={selectedCoworker.avatar}
+                  />
+                  <div
+                    className={`${styles.circle} ${
+                      selectedCoworker.status === "Online" ? styles.filled : ""
+                    }`}
+                  ></div>
+                </div>
+                <div className={styles.name}>{selectedCoworker.name}</div>
+              </div>
+              <div className={styles.coworkerComment}>
+                <div className={styles.profileCardBelow}>
+                  <div className={styles.profile}>
+                    <img
+                      className={styles.avatar}
+                      src={selectedCoworker.avatar}
+                      alt={selectedCoworker.avatar}
+                    />
+                    <div
+                      className={`${styles.circle} ${
+                        selectedCoworker.status === "Online"
+                          ? styles.filled
+                          : ""
+                      }`}
+                    ></div>
+                  </div>
+                  <div className={styles.nameComment}>
+                    <div className={styles.nameText}>
+                      {selectedCoworker.name}
+                    </div>
+                    <div className={styles.commentText}>
+                      {selectedCoworker.comment}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.sendMessage}>
+                {selectedCoworker.comment}
+              </div>
+            </div>
+          ) : (
+            <div className={styles.content}>
+              <div className={styles.defaultComment}>
+                World Class Developer Seolyu
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
